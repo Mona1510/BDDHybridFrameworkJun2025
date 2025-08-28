cb7259eca617a1d8f163c59941f5a04395b2f77a
Mona1510 <monabankar2910@gmail.com>
2025-08-29 00:01:04 +0530

    Added CaptureScreenshot

---
+++ src/test/java/com/qa/util/CaptureScreenshot.java
@@ -0,0 +1,25 @@
+package com.qa.util;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author DELL
+ * This class has common method to take the screenshot
+ *
+ */
+public class CaptureScreenshot {
+	
+	/**
+	 * @param driver
+	 * @return The object of captured screenshot in byte format
+	 */
+	public static byte[] captureImage(WebDriver driver)
+	{
+		TakesScreenshot srcShot= (TakesScreenshot) driver;
+		byte[] srcFile=srcShot.getScreenshotAs(OutputType.BYTES);
+		return srcFile;
+	}
+
+}
